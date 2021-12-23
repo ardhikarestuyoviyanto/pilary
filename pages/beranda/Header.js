0a20561e0436c563502be6e67915505c877465b1
Ardhikarestu <ardhikayoviyanto@gmail.com>
2021-12-23 20:55:44 +0700

    Fiks error link target blank

--- pages/beranda/Header.js
+++ pages/beranda/Header.js
@@ -15,7 +15,7 @@ export default function Headers(){
                             PT Pilar Teknologi, perusahaan yang selalu menerapkan prinsip kustomisasi (customization) demi mencapai kesuksesan setiap proyek. Kami telah mengelola ratusan proyek dari berbagai instansi pemerintah, perusahaan, kampus/ sekolah, NGO/ yayasan.
                             Kini saatnya Bisnis anda Go Online! Segera Upgrade Bisnis Anda dengan Website Profesional dengan pelayanan terlengkap, tidak perlu repot seting dan design karena kami menyiapkan semuanya. Terima beres!
                             <br/>
-                            <a href="https://api.whatsapp.com/send?phone=081326810290" type="button" target="_blank" className="bg-blue-600 mt-5 p-3 text-white rounded-md transition duration-500 ease-in-out hover:bg-blue-700 transform hover:-translate-y-1 hover:scale-110">
+                            <a href="https://api.whatsapp.com/send?phone=081326810290" type="button" className="bg-blue-600 mt-5 p-3 text-white rounded-md transition duration-500 ease-in-out hover:bg-blue-700 transform hover:-translate-y-1 hover:scale-110">
                                 <FontAwesomeIcon icon="phone-alt" className="mr-2"/>
                                 Hubungi Saya
                             </a>
